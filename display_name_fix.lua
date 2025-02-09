a4d9a3478e4564129b2ac1edb9f16e051a33f67d
kode-sec <kodescripts1@gmail.com>
2025-02-09 19:12:50 +0100

    Update display_name_fix.lua

--- display_name_fix.lua
+++ display_name_fix.lua
@@ -3,6 +3,8 @@ local oldDisplay = game:GetService("Players").LocalPlayer.DisplayName
 
 game:GetService("Players").LocalPlayer.DisplayName = ""
 
-repeat task.wait(1/10) until getgenv().LD_LOADED == true
+task.spawn(function()
+  repeat task.wait(1/10) until getgenv().LD_LOADED == true
 
-game:GetService("Players").LocalPlayer.DisplayName = oldDisplay
+  game:GetService("Players").LocalPlayer.DisplayName = oldDisplay
+end)
