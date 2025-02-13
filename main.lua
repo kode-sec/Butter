34f9513e6e359d9dab37b491bad753fb096bd6ff
Butterisgood <81777090+Butterisgood@users.noreply.github.com>
2025-02-13 13:49:16 -0500

    main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 13822889 then -- 🌳 Lumber Tycoon 2
 loadstring(game:HttpGet('https://raw.githubusercontent.com/kode-sec/Butter/refs/heads/main/LT2.lua'))("")
-elseif game.PlaceId == 6897226634 then -- Jailbreak
+elseif game.PlaceId == 606849621 then -- Jailbreak
 loadstring(game:HttpGet('https://raw.githubusercontent.com/kode-sec/Butter/refs/heads/main/Jailbreak.lua'))("")
 end
