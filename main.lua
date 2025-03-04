b03c4a911d235fc689924845ce0ffa538cea8146
Butterisgood <81777090+Butterisgood@users.noreply.github.com>
2025-03-04 12:51:54 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,4 +2,6 @@ if game.PlaceId == 13822889 then -- 🌳 Lumber Tycoon 2
 loadstring(game:HttpGet('https://raw.githubusercontent.com/kode-sec/Butter/refs/heads/main/LT2.lua'))("")
 elseif game.PlaceId == 606849621 then -- Jailbreak
 loadstring(game:HttpGet('https://raw.githubusercontent.com/kode-sec/Butter/refs/heads/main/Jailbreak.lua'))("")
+elseif game.PlaceId == 185655149 then -- Bloxburg
+loadstring(game:HttpGet('https://raw.githubusercontent.com/kode-sec/Butter/refs/heads/main/Bloxburg.lua'))("")
 end
