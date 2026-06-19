24d2a70b8f9a5a7199de9678614976f89f751485
kode <kodescripts1@gmail.com>
2026-06-19 16:48:23 +0200

    Implement key validation and UI for script access
    Added key validation and loading UI for key input.

--- main.lua
+++ main.lua
@@ -1,3 +1,10 @@
+local Key = getgenv().LDKey or getgenv().LD_KEY or script_key
+if Key == nil or Key == '' or Key == 'Key here' or Key == 'PUT KEY HERE' or type(Key) ~= 'string' then
+  getgenv().LD_KEY = ''
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/kode-sec/Butter/refs/heads/main/free_key_ui.lua"))()
+  repeat task.wait() until getgenv().LD_KEY ~= ''
+end
+
 if game.PlaceId == 13822889 then -- 🌳 Lumber Tycoon 2
 loadstring(game:HttpGet('https://raw.githubusercontent.com/kode-sec/Butter/refs/heads/main/LT2.lua'))("")
 elseif game.PlaceId == 537413528 then -- Build A Boat For Treasure
