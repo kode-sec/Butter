a3c05cdc189e89f96f838aab4615e18c86ef5f83
kode <kodescripts1@gmail.com>
2026-06-20 17:26:26 +0200

    Refactor key validation and loading logic

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local Key = getgenv().LDKey or getgenv().LD_KEY or script_key
 if Key == nil or Key == '' or Key == 'Key here' or Key == 'PUT KEY HERE' or type(Key) ~= 'string' then
-  getgenv().LD_KEY = ''
+  getgenv().LDKey ~= ''
   loadstring(game:HttpGet("https://raw.githubusercontent.com/kode-sec/Butter/refs/heads/main/free_key_ui.lua"))()
   repeat task.wait() until getgenv().LDKey ~= ''
 end
