fff08fa16a34442417cd640b45150c63868848ec
kode <kodescripts1@gmail.com>
2026-06-20 17:25:15 +0200

    Refactor key handling and game script loading

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ local Key = getgenv().LDKey or getgenv().LD_KEY or script_key
 if Key == nil or Key == '' or Key == 'Key here' or Key == 'PUT KEY HERE' or type(Key) ~= 'string' then
   getgenv().LD_KEY = ''
   loadstring(game:HttpGet("https://raw.githubusercontent.com/kode-sec/Butter/refs/heads/main/free_key_ui.lua"))()
-  repeat task.wait() until getgenv().LD_KEY ~= ''
+  repeat task.wait() until getgenv().LDKey ~= ''
 end
 
 if game.PlaceId == 13822889 then -- 🌳 Lumber Tycoon 2
