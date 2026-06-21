cb3bcf8919945a8344b0c9678c6780cd3e36cadf
Butter is good <81777090+Butterisgood@users.noreply.github.com>
2026-06-21 13:11:50 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,12 +7,10 @@ if Key == nil or Key == '' or Key == 'Key here' or Key == 'PUT KEY HERE' or type
   repeat task.wait() until getgenv().LDKey ~= ''
 end
 
-if game.PlaceId == 13822889 then -- 🌳 Lumber Tycoon 2
+if game.PlaceId == 13822889 then -- Lumber Tycoon 2 🌳
 loadstring(game:HttpGet('https://raw.githubusercontent.com/kode-sec/Butter/refs/heads/main/LT2.lua'))("")
-elseif game.PlaceId == 537413528 then -- Build A Boat For Treasure
+elseif game.PlaceId == 537413528 then -- Build A Boat For Treasure 🌊
 loadstring(game:HttpGet('https://raw.githubusercontent.com/kode-sec/Butter/refs/heads/main/ButterCooked.lua'))("")
-elseif game.PlaceId == 606849621 then -- Jailbreak
-loadstring(game:HttpGet('https://raw.githubusercontent.com/kode-sec/Butter/refs/heads/main/Jailbreak.lua'))("")
-elseif game.PlaceId == 185655149 then -- Bloxburg
-loadstring(game:HttpGet('https://raw.githubusercontent.com/kode-sec/Butter/refs/heads/main/Bloxburg.lua'))("")
+elseif game.PlaceId == 537413528 then -- Sell Lemons 🍋
+loadstring(game:HttpGet('https://raw.githubusercontent.com/kode-sec/Butter/refs/heads/main/Butter%20revolt%20%2B%20Key%20sys-obfuscated.lua'))("")
 end
