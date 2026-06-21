90b6a7ba2bf892a40e0c6e8a0cb1e962bc062208
Butter is good <81777090+Butterisgood@users.noreply.github.com>
2026-06-21 13:15:16 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -11,6 +11,6 @@ if game.PlaceId == 13822889 then -- Lumber Tycoon 2 🌳
 loadstring(game:HttpGet('https://raw.githubusercontent.com/kode-sec/Butter/refs/heads/main/LT2.lua'))("")
 elseif game.PlaceId == 537413528 then -- Build A Boat For Treasure 🌊
 loadstring(game:HttpGet('https://raw.githubusercontent.com/kode-sec/Butter/refs/heads/main/ButterCooked.lua'))("")
-elseif game.PlaceId == 537413528 then -- Sell Lemons 🍋
+elseif game.PlaceId == 79268393072444 then -- Sell Lemons 🍋
 loadstring(game:HttpGet('https://raw.githubusercontent.com/kode-sec/Butter/refs/heads/main/Butter%20revolt%20%2B%20Key%20sys-obfuscated.lua'))("")
 end
